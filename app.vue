a2612b084f5b7408828367d7139c75ea10d3edd6
nigelrmtaylor <31902847+nigelrmtaylor@users.noreply.github.com>
2024-11-30 15:40:16 +0100

    chore: update debug log message

--- app.vue
+++ app.vue
@@ -106,7 +106,7 @@ import type { Ref } from 'vue'
 import { useNuxtApp } from '#app'
 import { useDisplay } from 'vuetify'
 
-console.log('in app.vue');
+console.log('in app.vue...');
 
 // Add version tracking for cache debugging
 const APP_VERSION = '1.0.1';
