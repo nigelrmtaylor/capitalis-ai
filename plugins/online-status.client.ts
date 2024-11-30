219d549ca7e854b78904314e64fd0aaf3e95df54
nigelrmtaylor <31902847+nigelrmtaylor@users.noreply.github.com>
2024-11-30 15:28:24 +0100

    fix: add check for navigator.onLine property existence

--- plugins/online-status.client.ts
+++ plugins/online-status.client.ts
@@ -3,7 +3,10 @@ import { ref } from 'vue'
 export default defineNuxtPlugin(() => {
   const online = ref(true)
 
-  if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
+  if (typeof window !== 'undefined' && 
+      typeof navigator !== 'undefined' && 
+      'onLine' in navigator) {
+    
     online.value = navigator.onLine
     
     window.addEventListener('online', () => {
