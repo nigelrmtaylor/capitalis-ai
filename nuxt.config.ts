c698108dda852ed5c71347d2c543d23f5414e165
nigelrmtaylor <31902847+nigelrmtaylor@users.noreply.github.com>
2024-12-01 20:09:09 +0100

    Remove default Hanko API URL from nuxt config
    - Remove hardcoded fallback URL
    - API URL will now be provided by environment variable only

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -101,7 +101,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      hankoApi: process.env.NUXT_PUBLIC_HANKO_API_URL || 'https://46f53e73-2fea-4501-a0a4-5b285614b775.hanko.io',
+      hankoApi: process.env.NUXT_PUBLIC_HANKO_API_URL,
     }
   },
   typescript: {
