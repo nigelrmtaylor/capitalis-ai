377ccd373d368489ae8a9418d3c0923aa725a138
nigelrmtaylor <31902847+nigelrmtaylor@users.noreply.github.com>
2024-12-22 17:26:17 +0100

    committing before trying to create an icon

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,16 +1,54 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { md3 } from 'vuetify/blueprints'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    ssr: true,
+    blueprint: md3,
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
     theme: {
-      defaultTheme: 'light'
-    }
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FB8C00',
+          },
+        },
+        dark: {
+          dark: true,
+          colors: {
+            primary: '#2196F3',
+            secondary: '#424242',
+            accent: '#FF4081',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FB8C00',
+          },
+        },
+      },
+    },
   })
+
   app.vueApp.use(vuetify)
 })
